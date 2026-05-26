0ee876f61b1e5536ed5b0d22839fa0f1b933f59e
tamilanbann <72143916+tamilanbann@users.noreply.github.com>
2026-05-26 22:49:25 +0530

    added base url for vit app

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,      // change to your preferred port
+    open: true       // opens browser automatically
+  },
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
